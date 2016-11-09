83885aa7e1f592862707973fd51eeb38bb301eb2
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-11-09 14:40:50 +0300

    Release 0.1.0

--- src/lua/environ.lua
+++ src/lua/environ.lua
@@ -1,6 +1,6 @@
 local Environment = setmetatable({
   _NAME = 'environ';
-  _VERSION = '0.1.0-dev';
+  _VERSION = '0.1.0';
   _COPYRIGHT = 'Copyright (C) 2016 Alexey Melnichuk';
   _LICENSE = 'MIT';
 }, {__index = function(self, name)
