314ed89658881f9d9adf8aaf602526858ab8431f
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-11-09 15:06:40 +0300

    Back to dev version [ci skip]

--- src/lua/environ.lua
+++ src/lua/environ.lua
@@ -1,6 +1,6 @@
 local Environment = setmetatable({
   _NAME = 'environ';
-  _VERSION = '0.1.0';
+  _VERSION = '0.1.1-dev';
   _COPYRIGHT = 'Copyright (C) 2016 Alexey Melnichuk';
   _LICENSE = 'MIT';
 }, {__index = function(self, name)
